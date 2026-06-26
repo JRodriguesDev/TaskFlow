eaa2020533d40b6231b386f2f9aa1ed003343bec
José <0joserodrigues0@gmail.com>
2026-06-26 03:10:34 -0300

    feat: implementa validacao com safeParse do zod nas server actions de login e registro

--- src/app/(auth)/login/actions.ts
+++ src/app/(auth)/login/actions.ts
@@ -1,7 +1,18 @@
 'use server';
 
 import type { FormState } from '@/types/auth';
+import { loginSchema } from '@/lib/validations/auth';
 
 export const LoginAction = async (_prevState: FormState, form: FormData): Promise<FormState> => {
+  const validationFields = loginSchema.safeParse({
+    email: form.get('email'),
+    password: form.get('password'),
+  });
+
+  if (!validationFields.success) {
+    return { success: false, error: validationFields.error.issues[0].message };
+  }
+  const {email, password} = validationFields.data
+  
   return { success: true, error: null };
 };
--- src/app/(auth)/register/actions.ts
+++ src/app/(auth)/register/actions.ts
@@ -1,7 +1,19 @@
 'use server';
 
 import { FormState } from '@/types/auth';
+import {registerSchema} from '@/lib/validations/auth'
 
 export const registerAction = async (_prevState: FormState, form: FormData): Promise<FormState> => {
+  const validationFields = registerSchema.safeParse({
+    name: form.get('name'),
+    email: form.get('email'),
+    password: form.get('password'),
+    confirmPassword: form.get('confirmPassword')
+  })
+  if (!validationFields.success) {
+    return {success: false, error: validationFields.error.issues[0].message}
+  }
+  const {confirmPassword, ...userData} = validationFields.data
+
   return { success: true, error: null };
 };
